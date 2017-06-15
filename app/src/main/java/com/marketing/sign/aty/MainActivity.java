8a3067113e4867eba97aaeaab3125071cf49624e
shixueqiang <Shixueqiang@chinasofti.com>
2017-06-15 13:22:27 +0800

    add AdvancedWebView

--- app/src/main/java/com/marketing/sign/aty/MainActivity.java
+++ app/src/main/java/com/marketing/sign/aty/MainActivity.java
@@ -1,7 +1,9 @@
-package com.marketing.sign;
+package com.marketing.sign.aty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.marketing.sign.R;
 
 public class MainActivity extends AppCompatActivity {
 
